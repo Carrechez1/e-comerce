d0a056083c391db284d0a913e8bd113e07e24f7f
Alex7vzla <alejandro117925@gmail.com>
2022-09-06 16:46:14 -0400

    Navbar 2

--- src/components/Routes/Login.jsx
+++ src/components/Routes/Login.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import '../../css/login.css'
 
 const Login = () => {
-  return <div>Login</div>;
+  return ( 
+  <div>
+    <div id="sign-up">
+        <div className="container">
+            <span className="heading">Sign up</span>
+            <form>
+                <div className="group">
+                    <i className='bx bx-envelope' ></i>
+                    <input type="email" placeholder="Enter email"></input>
+                    <span className="col"></span>
+                </div>
+                <div className="group">
+                    <i className='bx bxs-lock'></i>
+                    <input type="password" placeholder="Enter password"></input>
+                    <span className="col"></span>
+                </div>
+                <div className="group">
+                    <button type="button">Log in</button>
+                </div>
+                <div className="member">
+                    <a href="#">Do you want to be a member? <span>Sign up</span></a>
+                </div>
+            </form>
+        </div>
+    </div>
+  </div>
+  )
 };
 
 export default Login;
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -5,21 +5,21 @@ import '../../css/header.css'
 const Header = () => {
   return (
     <header>
-        <a href="#" class="logo">E-commerce</a>
+        <a href="#" className="logo">E-commerce</a>
 
-        <div class="bx bx-menu" id="menu-icon"></div>
+        <div className="bx bx-menu" id="menu-icon"></div>
 
-        <ul class="navbar">
+        <ul className="navbar">
             <li><a href="#shop">Shop</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#faq">Faq</a></li>
             <li><a href="#contact">Contact</a></li>
         </ul>
         
-        <div class="icons">
-            <a href=""><i class='bx bx-search-alt-2'></i></a>
-            <a href=""><i class='bx bx-user'></i></a>
-            <a href=""><i class='bx bx-shopping-bag'></i></a>
+        <div className="icons">
+            <a href=""><i className='bx bx-search-alt-2'></i></a>
+            <a href=""><i className='bx bx-user'></i></a>
+            <a href=""><i className='bx bx-shopping-bag'></i></a>
         </div>
     </header>
   );
